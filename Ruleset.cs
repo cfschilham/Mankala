ef5c92f870a72ac4558c74e4587b3409a3b19253
JelleVisser01 <jellevisser.01@gmail.com>
2023-10-20 18:44:15 +0200

    ApplyMove

--- Ruleset.cs
+++ Ruleset.cs
@@ -3,7 +3,7 @@
 public interface Ruleset
 {
     Cup[] GenerateCups();
-    int ApplyMove(int index, Cup[] state);
+    Player ApplyMove(int index, Cup[] state, Player current);
     int Winner(Cup[] state);
 }
 
@@ -14,9 +14,50 @@ public class MankalaRules : Ruleset
         throw new NotImplementedException();
     }
 
-    public int ApplyMove(int index, Cup[] state)
+    public Player ApplyMove(int index, Cup[] state, Player current)
     {
-        throw new NotImplementedException();
+        int Currentpebbles = state[index].Pebbles;
+        state[index].Pebbles = 0;
+
+        while(Currentpebbles > 0) 
+        {
+            if (index < state.Length - 1) 
+            {
+                index++;
+                if (index != 6 && index != 13 || state[index].Owner == current)
+                {
+                    state[index].Pebbles++;
+                    Currentpebbles--;
+                }
+            }
+            else 
+            {
+                index = 0;
+                if (state[index].Owner == current)
+                {
+                    state[index].Pebbles++;
+                    Currentpebbles--;
+                }
+            }
+        }
+
+        if (state[index].Owner == current && (index == 6 || index == 13))
+            return current;
+
+        if (state[index].Pebbles - 1 != 0)
+            return ApplyMove(index, state, current);
+
+        if (state[index].Owner == current && (state[12 - index].Pebbles != 0))
+        {
+            int newPebbles = state[12 - index].Pebbles + 1;
+            state[12 - index].Pebbles = 0; state[index].Pebbles = 0;
+            if (state[6].Owner == current)
+                state[6].Pebbles += newPebbles;
+            else
+                state[13].Pebbles += newPebbles;
+        }
+
+        return state[index].Owner; 
     }
 
     public int Winner(Cup[] state)
